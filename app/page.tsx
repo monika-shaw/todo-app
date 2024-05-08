9d0f19c48ca079e8ce375ef044d12c57b1476988
shawmonika616ms <shawmonika616ms@gmail.com>
2024-05-08 22:57:53 +0530

    Description : installed daisy UI for tailwind css.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Hello</h1>
-    </main>
-  );
+    return (
+        <main>
+            <h1 className="text-center underline">Hello</h1>
+            <button className="btn">Click me</button>
+        </main>
+    );
 }
