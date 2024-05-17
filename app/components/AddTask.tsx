41ef0b96810f18fa509962f0e2ec9c0cf8029fd7
shawmonika616ms <shawmonika616ms@gmail.com>
2024-05-17 14:35:18 +0530

    Description : added modal component. Developer : Monika.

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,8 +1,13 @@
+"use client"
+import { useState } from "react"
+import Modal from "./Modal"
 
 function AddTask() {
+  const [modalOpen, setmodalOpen] = useState<boolean>(false)
   return (
     <div>
-        <button className="btn btn-primary w-full">Add Task</button>
+        <button className="btn btn-primary w-full" onClick={()=>setmodalOpen(true)}>Add Task</button>
+        <Modal modalOpen = {modalOpen} setmodalOpen={setmodalOpen}/>
     </div>
   )
 }
